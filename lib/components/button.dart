32e3692e2b9ffacc808bc077afadceed77dd9d04
Matheus Araujo <matheus-araujo2001@hotmail.com>
2022-07-04 16:05:46 -0300

    init

---
+++ lib/components/button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+
+  final String text;
+
+  Button({
+    @required this.text,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(text),
+      onPressed: () {},
+    );
+  }
+}
---
+++ lib/components/keyboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'button_row.dart';
+import 'button.dart';
+
+
+class Keyboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      child: Column(
+        children: <Widget>[
+         
+        ],
+      ),
+    );
+  }
+}
